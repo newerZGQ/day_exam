b3293df8919dc6a9b3806dafff55a5822bde2406
alt99 <14215786+newerZGQ@users.noreply.github.com>
2025-12-05 08:26:35 +0000

    feat: 移除“保持屏幕常亮”功能及相关设置

--- app/src/main/java/com/gorden/dayexam/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/gorden/dayexam/ui/settings/SettingsFragment.kt
@@ -3,25 +3,16 @@ package com.gorden.dayexam.ui.settings
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.WindowManager
-import android.widget.Toast
+
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import com.gorden.dayexam.ContextHolder
+ 
 import com.gorden.dayexam.R
 import com.gorden.dayexam.ui.dialog.EditTextDialog
 
 class SettingsFragment: PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_preference, rootKey)
-        findPreference<Preference>(resources.getString(R.string.keep_screen_light_key))?.setOnPreferenceChangeListener { preference, newValue ->
-            if (newValue == true) {
-                requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-            } else {
-                requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-            }
-            return@setOnPreferenceChangeListener true
-        }
         findPreference<Preference>(resources.getString(R.string.privacy_key))?.setOnPreferenceClickListener {
             EditTextDialog(requireActivity(),
                 requireActivity().resources.getString(R.string.privacy_dialog_title),
